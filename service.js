a1bcbe2c769e14ecbc54af8587c91555e268bf25
Xaekai <ribbon13@gmail.com>
2019-07-25 16:05:28 -0700

    Improve handling of missing config file

--- service.js
+++ service.js
@@ -11,8 +11,27 @@
 
 const PonkBot = require('./lib/ponkbot.js');
 
-const argv = require('minimist')(process.argv.slice(2));
-const configfile = typeof argv.config == "String" ? argv.config : "./config";
+function getConfig(args){
+    let config = './config.js';
+    config = typeof args.config == "string" ? args.config : config;
 
+    // Check for relative path without leading "./"
+    if( !config.match(/^\//) && config.match(/\//)){
+        config = `./${config}`;
+    }
+
+    try {
+        require.resolve(config);
+    }
+    catch(err) {
+        console.error('Could not locate configuration file');
+        process.exit(78);
+    }
+
+    return config;
+}
+
+const argv = require('minimist')(process.argv.slice(2));
+const configfile = getConfig(argv);
 const config = require(configfile);
 const bot = new PonkBot(config);
