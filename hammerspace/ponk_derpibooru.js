7650c1a494bed7fed41b89d28efc4bf0de1ba64a
Xaekai <ribbon13@gmail.com>
2019-07-30 17:36:16 -0700

    Derpibooru search command

--- hammerspace/ponk_derpibooru.js
+++ hammerspace/ponk_derpibooru.js
@@ -15,10 +15,12 @@ class Derpibooru {
             pool    : new require('https').Agent({ maxSockets: 2 }),
             timeout : 15 * 1000,
 
+            domain     : 'www.derpibooru.org',
             errorcode  : ' ',
             embed      : '.pic',
             embedLarge : '.picl',
             spoiler    : '.spl',
+            maxPages   : 5,
 
             useSpoiler  : true,
             useSmall    : true,
@@ -43,7 +45,7 @@ class Derpibooru {
 
     getImageDataByID(imageID){
         return new Promise((resolve, reject)=>{
-            const url = `https://derpibooru.org/${imageID}.json?key=${this.key}`
+            const url = `https://${this.domain}/${imageID}.json?key=${this.key}`
             request(Object.assign({ url }, this.requestOpts), (error, response, body) => {
                 if(error){
                     return reject(error);
@@ -52,10 +54,9 @@ class Derpibooru {
                     return reject(response.statusCode === 400 ? 'Bad Request' : 'Unknown');
                 }
 
-                let result = JSON.parse(body);
+                const result = JSON.parse(body);
 
-                // { "status":"500", "error":"Internal Server Error" }
-                if(result["error"]){
+                if(result["error"]){ // { "status":"500", "error":"Internal Server Error" }
                     return reject(result["error"]);
                 }
 
@@ -68,6 +69,59 @@ class Derpibooru {
         });
     }
 
+
+    // https://derpibooru.org/search.json?q=pinkie+pie
+    search(queryText) {
+        const getRequest = (url) => {
+            return new Promise((resolve, reject)=>{
+                request(Object.assign({ url }, this.requestOpts), (error, response, body) => {
+                    if(error){ 
+                        return reject(error);
+                    }
+                    if(response.statusCode !== 200){ 
+                        return reject(response.statusCode === 400 ? 'Bad Request' : 'Unknown');
+                    }
+                    const result = JSON.parse(body);
+
+                    if(result["error"]){ // { "status":"500", "error":"Internal Server Error" }
+                        return reject(result["error"]);
+                    }
+
+                    return resolve(result);
+                });
+            });
+        }
+
+        return new Promise((resolve, reject)=>{
+            const query = encodeURIComponent(queryText.split(',').filter((e) => { return e.trim().length}).join(','));
+            if(!query.length){
+                return reject('Empty search query');
+            }
+
+            // first lets get how many results this is gonna get
+            const queryTotal = `https://${this.domain}/search.json?key=${this.key}&q=${query}&perpage=1`
+            getRequest(queryTotal).then((result)=>{
+                const results = result.total;
+                if(results === 0){
+                    return reject('No results');
+                }
+
+                const totalPages = Math.ceil(results / 50);
+                let pages = Math.min(this.maxPages, totalPages);
+
+                const queries = [];
+
+                while(pages--){
+                    const page = pages+1;
+                    const url = `https://${this.domain}/search.json?key=${this.key}&q=${query}&sf=score&sd=desc&page=${page}&perpage=50`
+                    queries.unshift(getRequest(url));
+                }
+                Promise.all(queries).then((searchResults)=>{
+                    return resolve(searchResults.map(page => page.search).flat());
+                });
+            });
+        });
+    }
 
 
     /*
@@ -85,7 +139,7 @@ class Derpibooru {
         }
 
         this.checkCooldown({
-            type: 'derpibooru', user
+            type: 'derpibooru', user, modBypass: this.getUserRank(user) > 2
         }).then(()=>{
 
             const postAPI = (imageData)=>{
@@ -100,9 +154,36 @@ class Derpibooru {
         },(message)=>{
             this.sendPrivate(message, user);
         });
-
     }
 
+    handleDerpi(user, params, meta){
+        if (this.muted || !this.API.derpibooru || !params){
+            return
+        }
+
+        const query = params.trim();
+        if(!query){
+            return this.sendPrivate('[Derpibooru] { Error: Invalid Command Syntax }', username);
+        }
+
+        this.checkCooldown({
+            type: 'derpibooru', user, modBypass: this.getUserRank(user) > 2
+        }).then(()=>{
+
+            const postAPI = (resultSet)=>{
+                const imageData = resultSet[Math.floor(Math.random() * resultSet.length)];
+                this.sendMessage(`[Derpibooru] { Results: ${resultSet.length} }\n https://${imageData.representations.small}${this.API.derpibooru.embed}`);
+            };
+
+            const handleError = (err)=>{
+                this.sendMessage(`[Derpibooru] Something went wrong. ${err}`);
+            }
+
+            this.API.derpibooru.search(query).then(postAPI, handleError);
+        },(message)=>{
+            this.sendPrivate(message, user);
+        });
+    }
 }
 
 
@@ -123,6 +204,9 @@ module.exports = {
         'booru': function(user, params, meta){
             this.API.derpibooru.handleBooru.call(this, user, params, meta);
         },
+        'derpi': function(user, params, meta){
+            this.API.derpibooru.handleDerpi.call(this, user, params, meta);
+        },
     },
     cooldowns: {
         derpibooru: {
@@ -135,4 +219,3 @@ module.exports = {
         }
     },
 }
-
